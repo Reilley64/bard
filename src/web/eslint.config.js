9e2fa319be0253eb1bf31efc6bd0f3fbb53d7c5f
Reilley <reilleygray@gmail.com>
2024-09-04 16:07:51 +1000

    Switch InputProps to type alias and add Tailwind CSS ESLint plugin
    Refactor InputProps to use a type alias for cleaner code. Also, integrate the Tailwind CSS ESLint plugin for improved style enforcement in the project.

--- src/web/eslint.config.js
+++ src/web/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import tailwindcss from 'eslint-plugin-tailwindcss'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -16,6 +17,7 @@ export default tseslint.config(
         plugins: {
             'react-hooks': reactHooks,
             'react-refresh': reactRefresh,
+            'tailwindcss': tailwindcss,
         },
         rules: {
             ...reactHooks.configs.recommended.rules,
