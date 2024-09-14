f23e68f070439e11611c09e2a99927bacb37630a
Reilley <reilleygray@gmail.com>
2024-09-14 17:19:25 +1000

    Update server initialization to use dynamic host
    Changed the server initialization to dynamically use the host from `window.location.host` instead of hardcoding "localhost:3000". This allows the server to work correctly in different environments without requiring code changes.

--- src/web/src/lib/server.ts
+++ src/web/src/lib/server.ts
@@ -1,6 +1,6 @@
 import { type App } from "../../../index.ts";
 import { treaty } from "@elysiajs/eden";
 
-const server = treaty<App>("localhost:3000");
+const server = treaty<App>(window.location.protocol + "//" + window.location.host);
 
 export default server;
